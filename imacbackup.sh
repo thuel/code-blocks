552dbd41cb0e3aaf289748b582b8972e87215b28
Lukas Steffen <lukas@steffen-steffen.ch>
2018-11-30 12:48:03 +0100

    real command to finish backup of imac

--- imacbackup.sh
+++ imacbackup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-logf=~/Desktop/imacbackup.log
+logf=~/Desktop/imacbackup_real.log
 
-sudo rsync -navhH --numeric-ids --exclude='/Users/Fritz/Virtual**' --exclude='/Users/Fritz/Movie**' --exclude='/Users/Fritz/iMovie**' --exclude='/Users/Fritz/Download**' --exclude='/Users**' --exclude='/Applications**' /Volumes/HD/ /Volumes/Archiv-002/Archiv-002/2018-11-25-iMacHD/custom/ >> $logf
+sudo rsync -navhH --numeric-ids --exclude='/Users/Fritz/Virtual**' --exclude='/Users/Fritz/Movie**' --exclude='/Users/Fritz/iMovie**' --exclude='/Users/Fritz/Download**' /Volumes/HD/ /Volumes/Archiv-002/Archiv-002/2018-11-25-iMacHD/custom/ >> $logf
